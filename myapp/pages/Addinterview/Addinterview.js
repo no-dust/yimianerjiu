0698bc29101ff0cfa704c764889adfaafc61d67a
no-dust <3343748251@qq.com>
2019-04-22 13:51:22 +0800

    重新

--- myapp/pages/Addinterview/Addinterview.js
+++ myapp/pages/Addinterview/Addinterview.js
@@ -6,7 +6,10 @@ Page({
    */
   data: {
     date: '2019-04-20',
-    time: '12:01'
+    time: '12:01',
+    current:[{
+      company:""
+    }]
   },
   bindDateChange(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -19,6 +22,22 @@ Page({
     this.setData({
       time: e.detail.value
     })
+  },
+  // 按钮是否可点击
+  btnEnable() {
+    // 判断公司名称是否为空
+    if (!this.current.company) {
+      return false;
+    }
+    // 判断手机号是否符合规范
+    if (!/^1(3|4|5|7|8)\d{9}$/.test(this.current.phone) || !/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(this.current.phone)) {
+      return false;
+    }
+    // 判断公司地址
+    if (!this.current.address.address) {
+      return false;
+    }
+    return true;
   },
   /**
    * 生命周期函数--监听页面加载
